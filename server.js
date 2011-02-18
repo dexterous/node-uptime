30a11bf095cef46783f1c5981412b276d1ef9921
Saager Mhatre <saager.mhatre@gmail.com>
2011-02-18 12:04:22 +0530

    damn node and it's experimental status; os.uptime() doesn't work on sunos

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var os = require('os')
   , http = require('http')
 
+var start = new Date()
+
 http.createServer(function(req, res) {
-  res.end(os.hostname() + " has been up for " + os.uptime())
+  res.end(os.hostname() + " has been up for " + new Date() - start)
 }).listen(80)
 
