de5aa3a9cfa1d0f396ecd4e8508f67c0e91bbdf7
Saager Mhatre <saager.mhatre@gmail.com>
2011-02-18 11:55:28 +0530

    damn node and it's crummy docs

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var os = require('os')
   , http = require('http')
 
 http.createServer(function(req, res) {
-  res.end(os.hostname + " has been up for " + os.uptime)
+  res.end(os.hostname() + " has been up for " + os.uptime())
 }).listen(80)
 
